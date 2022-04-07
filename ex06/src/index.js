166f20cf53649f9153e75b639c6a0bc1f61b63e8
opajkovic <oljaolja170@gmail.com>
2022-04-07 20:18:35 +0200

    added ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,7 @@
+var myNestedArray = [["Toblerone", 5], ["Milka", 3]];
+function myNestedFunction(arr) {
+    var myNestedArray = [...arr];
+    return myNestedArray;
+}
+console.log(myNestedFunction(myNestedArray));
+module.exports = myNestedFunction;
